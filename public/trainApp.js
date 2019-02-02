131ac6a41e3c6741ac254371435c1a180d32c33e
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-02-02 17:47:00 -0500

    Authentication integration attempts v0.01

--- public/trainApp.js
+++ public/trainApp.js
@@ -8,6 +8,17 @@ const trainData = $("#trainData");
 const currentTime = $("#currentTime");
 // let dropdownFilled = false;
 // var provider = new firebase.auth.GithubAuthProvider();
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBqqVykbzogd6LI1YTGO6PmUbuH3VzsiZE",
+  authDomain: "traintracker-e3faf.firebaseapp.com",
+  databaseURL: "https://traintracker-e3faf.firebaseio.com",
+  projectId: "traintracker-e3faf",
+  storageBucket: "traintracker-e3faf.appspot.com",
+  messagingSenderId: "416740466914"
+};
+firebase.initializeApp(config);
+const database = firebase.database();
 
 //initialize Materialize form
 M.AutoInit();
@@ -15,49 +26,54 @@ $(".select-dropdown").on("contentChanged", function(event) {
     $(this).formSelect();
 });
 
-// var uiConfig = {
-//     signInSuccessUrl: '<url-to-redirect-to-on-success>',
-//     signInOptions: [
-//       // Leave the lines as is for the providers you want to offer your users.
-//     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-//     //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-//       firebase.auth.GithubAuthProvider.PROVIDER_ID,
-//     //   firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//     //   firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-//     //   firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-//     ],
-//     // tosUrl and privacyPolicyUrl accept either url string or a callback
-//     // function.
-//     // Terms of service url/callback.
-//     tosUrl: '<your-tos-url>',
-//     // Privacy policy url/callback.
-//     privacyPolicyUrl: function() {
-//       window.location.assign('<your-privacy-policy-url>');
-//     }
-//   };
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+          // User successfully signed in.
+          return redirectUrl;
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return true;
+        },
+        uiShown: function() {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none';
+        }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow: 'redirect',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+    //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    //   firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    //   firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function() {
+      window.location.assign('<your-privacy-policy-url>');
+    }
+  };
 
-//   // Initialize the FirebaseUI Widget using Firebase.
-//   var ui = new firebaseui.auth.AuthUI(firebase.auth());
-//   // The start method will wait until the DOM is loaded.
-//   ui.start('#firebaseui-auth-container', uiConfig);
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
 
 
 
 
 // var instance = M.Dropdown.getInstance();
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBqqVykbzogd6LI1YTGO6PmUbuH3VzsiZE",
-    authDomain: "traintracker-e3faf.firebaseapp.com",
-    databaseURL: "https://traintracker-e3faf.firebaseio.com",
-    projectId: "traintracker-e3faf",
-    storageBucket: "traintracker-e3faf.appspot.com",
-    messagingSenderId: "416740466914"
-  };
-  firebase.initializeApp(config);
-  const database = firebase.database();
   
 // $(document).on("click", ".select-dropdown", function(event) {
 //       console.log(event);
